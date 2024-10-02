45b94de1378e26ea3e4539d6a7d30d05d4fef750
hardik072 <hardikpatel63319@gmail.com>
2024-10-02 22:41:41 +0300

    Add message here

---
+++ activity6.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const os = require('os');
+
+console.log('Hostname:', os.hostname());
+
+console.log('OS Platform:', os.platform());
+
+console.log('CPU Cores:', os.cpus());
+
